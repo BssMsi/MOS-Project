9c6ce3c74b79a146fce2a5f2b59de891c62ad727
BssMsi <bharathsshroff@gmail.com>
2016-12-04 13:21:03 +0530

    4Dec

--- Code.py
+++ Code.py
@@ -1,5 +1,5 @@
 from Tkinter import Tk, Frame, Button, Label, Scrollbar, N, S, E, W, HORIZONTAL, StringVar, Canvas, Entry, SUNKEN, \
-    IntVar, Radiobutton
+    IntVar, Radiobutton, RAISED
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plot
 from matplotlib import gridspec
@@ -25,6 +25,7 @@ class AutoScrollbar(Scrollbar):
 
 class PlotGraph:
     breadth = 1.0
+    MAX_STRESS = 44
 
     def __init__(self):
         def onresize(event):
@@ -39,30 +40,40 @@ class PlotGraph:
         self.root.wm_title("Micro-Tensile Testing Machine")
         self.root.state("zoomed")
 
-        ########################################################################################################################
+########################################################################################################################
         # Title
         header = Frame(self.root)
-        Label(header, text="Micro-Tensile Testing Machine", font=("Helvetica", 40, "bold")).grid(row=0)
+        Label(header, text="Micro-Tensile Testing Machine", font=("Helvetica", 40, "bold"), bg="green", relief=RAISED).grid(row=0)
         header.columnconfigure(0, weight=1)
         header.grid(row=0, sticky=E + W)
-        ########################################################################################################################
+########################################################################################################################
         # Parameters
         par_frame = Frame(self.root)
+        choice_frame = Frame(par_frame, bd=2, relief=RAISED)
         self.choice = IntVar()
         self.choice.set(1)
-        Radiobutton(par_frame, text="Elongation", variable=self.choice, value=1).grid(row=0, column=0, padx=50)
-        Radiobutton(par_frame, text="Bending", variable=self.choice, value=2).grid(row=1, column=0, padx=50)
+        Radiobutton(choice_frame, text="ELONGATION", font=("Courier", 15),
+                    variable=self.choice, value=1).grid(row=0, column=0, padx=50)
+        Radiobutton(choice_frame, text="BENDING", font=("Courier", 15),
+                    variable=self.choice, value=2).grid(row=1, column=0, padx=50)
+        choice_frame.grid(row=0, column=0, sticky=W)
 
-        Label(par_frame, text="LENGTH(mm)", relief=SUNKEN).grid(row=0, column=1, ipadx=5, ipady=1)
-        self.L = Entry(par_frame)
+        spec_frame = Frame(par_frame, bd=2, relief=RAISED)
+        Label(spec_frame, text="Specimen\nDimensions", font=("arial", 15)).grid(row=0, column=0, rowspan=2)
+        Label(spec_frame, text="LENGTH(mm)", font=("Courier", 15),
+              relief=SUNKEN, bg="yellow", fg="brown").grid(row=0, column=1, ipadx=5, ipady=1)
+        self.L = Entry(spec_frame, fg="blue", font=("Courier", 15), width=7)
         self.L.insert(0, 25.4)
         self.L.grid(row=0, column=2)
-        Label(par_frame, text="Area(mm^2)", relief=SUNKEN).grid(row=1, column=1, ipadx=5, ipady=1)
-        self.area_box = Entry(par_frame)
+        Label(spec_frame, text="Area(mm^2)", font=("Courier", 15),
+              relief=SUNKEN, bg="yellow", fg="brown").grid(row=1, column=1, ipadx=5, ipady=1)
+        self.area_box = Entry(spec_frame, fg="blue", font=("Courier", 15), width=7)
         self.area_box.insert(0, 40.3765)
         self.area_box.grid(row=1, column=2)
+        spec_frame.grid(row=0, column=1, rowspan=2, sticky=E)
+        par_frame.grid_columnconfigure(1, weight=1)
         par_frame.grid(row=1, sticky=E + W)
-        ########################################################################################################################
+########################################################################################################################
         # Main Frame containing Curve and data display
         curve = Frame(self.root)
 
@@ -85,7 +96,7 @@ class PlotGraph:
         live_plot.set_xlim([0, float(self.L.get())])
         live_plot.set_ylim([0, 50.0])  # Set y axis limits for live diagram
 
-        self.lines = live_plot.plot([0.0], [0.0], 'k-', [0.0], [0.0], 'r--')
+        self.lines = live_plot.plot([0.0], [0.0], 'b-', [0.0], [0.0], 'r--')
         self.live_plot = live_plot
         ###################################################################
         # # Stress-Strain Curve
@@ -108,7 +119,7 @@ class PlotGraph:
         self.canvas_main.get_tk_widget().grid(row=0, column=0, sticky=N + S + E + W)
 
         # Displaying data
-        disp_data = Frame(curve)
+        disp_data = Frame(curve, relief=RAISED, bd=2)
         yscrollbar = AutoScrollbar(disp_data)
         yscrollbar.grid(row=0, column=1, rowspan=4, sticky=N + S)
         xscrollbar = AutoScrollbar(disp_data, orient=HORIZONTAL)
@@ -119,11 +130,12 @@ class PlotGraph:
         xscrollbar.config(command=data_canvas.xview)
         label_frame = Frame(disp_data)
         Label(label_frame, text="DATA\nS.No  Load(N)  Elongation(mm)\n",
-              font=("Helvetica", 17, "bold")).grid(row=0, column=0, sticky=N + S + W + E)
+              font=("COMIC SANS MS", 17, "bold")).grid(row=0, column=0, sticky=N + S + W + E)
         self.data = StringVar()
         self.data_str = ""
         self.data.set(self.data_str)
-        Label(label_frame, textvariable=self.data, font=16).grid(row=1, column=0, sticky=N + S + W + E)
+        Label(label_frame, textvariable=self.data,
+              font=("Helvetica", 16, "italic")).grid(row=1, column=0, sticky=N + S + W + E)
         data_canvas.create_window(0, 0, window=label_frame)
         label_frame.bind("<Configure>", onresize)
         label_frame.bind_all("<MouseWheel>", on_mousewheel)
@@ -136,7 +148,7 @@ class PlotGraph:
 
         curve.columnconfigure(0, weight=1)
         curve.grid(row=2, sticky=N + S + E + W)
-        ########################################################################################################################
+########################################################################################################################
         footer = Frame(self.root)
         Button(footer, text="Run", font=('Comic Sans MS', 25, "bold italic"),
                command=self.run_program).grid(row=0, column=0)
@@ -150,40 +162,38 @@ class PlotGraph:
         # Initialize
         # # Reset all values to initial value
         try:
+            self.arrow1.remove()
+            self.arrow2.remove()
+            self.label1.remove()
+            self.label2.remove()
+
+            self.material_type.remove()
             self.zoom.remove()
             self.load.remove()
             self.load_arrow.remove()
             self.delta.remove()
             self.del_arrow.remove()
 
-            self.arrow1.remove()
-            self.arrow2.remove()
-            self.label1.remove()
-            self.label2.remove()
         except AttributeError:
             # If this is first run, above values have not been initialized, hence AttributeError will be raised
             pass
 
+        self.data_str = ""
         self.no_reading = 0
         L = float(self.L.get())
 
         self.stress = np.array([0.0])
         self.strain = np.array([0.0])
         self.points[0].set_data(self.strain, self.stress)
-        if self.choice.get() == 1:
-            self.curve_plot.set_title("Stress-Strain Curve")
-            self.curve_plot.set_xlabel("STRAIN ->")
-            self.curve_plot.set_ylabel("STRESS (MPa) ->")
-        else:
-            self.curve_plot.set_title("Force-Displacement Curve")
-            self.curve_plot.set_xlabel("DISPLACEMENT (mm) ->")
-            self.curve_plot.set_ylabel("FORCE (N) ->")
 
         x_shift = 3.0
 
         self.live_plot.set_xlim([0, L + x_shift + 5.0])
         if self.choice.get() == 1:
             y_shift = 10.0
+            self.curve_plot.set_title("Stress-Strain Curve")
+            self.curve_plot.set_xlabel("STRAIN ->")
+            self.curve_plot.set_ylabel("STRESS (MPa) ->")
             self.live_plot.set_ylim([0, 50.0])
             self.lines[0].set_data([x_shift, L + x_shift, L + x_shift, x_shift, x_shift],
                                    [y_shift, y_shift, y_shift + self.breadth, y_shift + self.breadth, y_shift])
@@ -192,7 +202,7 @@ class PlotGraph:
             self.zoom = zoomed_inset_axes(self.live_plot, 6)
             self.zoom_lines = self.zoom.plot([x_shift, L + x_shift, L + x_shift, x_shift, x_shift],
                                              [y_shift, y_shift, y_shift + self.breadth, y_shift + self.breadth,
-                                              y_shift], 'k-',
+                                              y_shift], 'b-',
                                              [0], [0], 'r--')
             self.zoom.grid(True)
             self.zoom.set_xlim([xlim1, xlim2])
@@ -206,6 +216,9 @@ class PlotGraph:
             mark_inset(self.live_plot, self.zoom, loc1=3, loc2=4, fc="none", ec="0.5")
         else:
             y_shift = 1.0
+            self.curve_plot.set_title("Force-Displacement Curve")
+            self.curve_plot.set_xlabel("DISPLACEMENT (mm) ->")
+            self.curve_plot.set_ylabel("FORCE (N) ->")
             self.lines[0].set_data([x_shift, L + x_shift, L + x_shift, x_shift, x_shift],
                                    [y_shift, y_shift, y_shift + self.breadth / 10, y_shift + self.breadth / 10,
                                     y_shift])
@@ -229,31 +242,36 @@ class PlotGraph:
         f.close()
         ind = np.argmax(self.stress)
 
-        self.arrow1 = self.curve_plot.annotate('', xy=(self.strain[ind], self.stress[ind]),
-                                               xytext=(self.strain[ind], 0),
-                                               arrowprops=dict(arrowstyle="<->"))
-        self.label1 = self.curve_plot.text(self.strain[ind] + 0.001, self.stress[ind] / 2, 'Tensile Strength')
-        self.arrow2 = self.curve_plot.annotate('', xy=(self.strain[self.no_reading], self.stress[self.no_reading]),
-                                               xytext=(self.strain[self.no_reading], 0),
-                                               arrowprops=dict(arrowstyle="<->"))
-        self.label2 = self.curve_plot.text(self.strain[self.no_reading] + 0.001,
-                                           self.stress[self.no_reading] / 2, 'Fracture')
+        if self.choice.get() == 1:
+            self.arrow1 = self.curve_plot.annotate('', xy=(self.strain[ind], self.stress[ind]),
+                                                   xytext=(self.strain[ind], 0),
+                                                   arrowprops=dict(arrowstyle="<->"))
+            self.label1 = self.curve_plot.text(self.strain[ind] + 0.001, self.stress[ind] / 2, 'Ultimate Strength')
+            self.arrow2 = self.curve_plot.annotate('', xy=(self.strain[self.no_reading], self.stress[self.no_reading]),
+                                                   xytext=(self.strain[self.no_reading], 0),
+                                                   arrowprops=dict(arrowstyle="<->"))
+            self.label2 = self.curve_plot.text(self.strain[self.no_reading] + 0.001,
+                                               self.stress[self.no_reading] / 2, 'Fracture')
+
+            self.material_type = self.curve_plot.text(self.curve_plot.get_xlim()[1] / 2, self.stress[ind] + 100
+                                                      , 'Ductile Material', fontsize=15,
+                                                      color='black', bbox=dict(facecolor='red', edgecolor='black'
+                                                                               , boxstyle='round'))
 
         self.main_curve.canvas.draw()
         self.root.update_idletasks()
 
     def update_plot(self, load, delta_l, L, x_shift, y_shift):
         # Plot graph
-        if self.no_reading > 1:
-            print (load - (self.stress[self.no_reading]) / (delta_l - self.strain[self.no_reading])), "-", load
-
+        
         self.no_reading += 1
-        self.data_str += '{:3}.\t{:.1f}\t{:=1.6f}\n'.format(self.no_reading, load, delta_l)
+        self.data_str += '{:3}.\t{:.1f} \t {:=1.6f}\n'.format(self.no_reading, load, delta_l)
         self.data.set(self.data_str)
 
         # Update live diagram
         # # Elongation
         if self.choice.get() == 1:
+            # # Update Live Diagram
             self.lines[1].set_data([L + x_shift, L + x_shift + delta_l, L + x_shift +
                                     delta_l, L + x_shift],
                                    [y_shift, y_shift, y_shift + self.breadth, y_shift + self.breadth])
@@ -265,9 +283,22 @@ class PlotGraph:
             self.load_arrow.xy = (L + x_shift + delta_l + 0.25, y_shift + self.breadth / 2)
             self.load_arrow.xytext = (L + x_shift + delta_l, y_shift + self.breadth / 2)
 
+            # # Update Stress-Strain Curve
+            stress = load / float(self.area_box.get())
+            strain = delta_l / L
+
+            self.stress = np.append(self.stress, [stress])
+            self.strain = np.append(self.strain, [strain])
+
+            self.points[0].set_data(self.strain, self.stress)
+            if stress >= np.max(self.stress):
+                self.curve_plot.set_ylim([0, stress + 0.2 * stress])
+            self.curve_plot.set_xlim([0, strain + 0.2 * strain])
+
         # # Bending
         # ################ IMP: Note equation used for bending is an approximation for visualization purposes ##########
         elif self.choice.get() == 2:
+            # # Update Live Diagram
             # Assume equation f(x) = px2 + qx + r
             # f(x_shift) = y_shift
             # f(x_shift + L) = y_shift
@@ -289,24 +320,27 @@ class PlotGraph:
             self.lines[1].set_data(np.append(x_points, np.flipud(x_points) + self.breadth / 10),
                                    np.append(y_points, np.flipud(y_points) + self.breadth / 10))
 
-        # Update Stress-Strain Curve
-        stress = load / float(self.area_box.get())
-        strain = delta_l / L
+            # # Update Force-Displacement Curve
+            self.stress = np.append(self.stress, [load])
+            self.strain = np.append(self.strain, [delta_l])
 
-        self.stress = np.append(self.stress, [stress])
-        self.strain = np.append(self.strain, [strain])
-
-        self.points[0].set_data(self.strain, self.stress)
-        if stress >= np.max(self.stress):
-            self.curve_plot.set_ylim([0, stress + 0.2 * stress])
-        self.curve_plot.set_xlim([0, strain + 0.2 * strain])
+            self.points[0].set_data(self.strain, self.stress)
+            if load >= np.max(self.stress):
+                self.curve_plot.set_ylim([0, load + 0.2 * load])
+            self.curve_plot.set_xlim([0, delta_l + 0.2 * delta_l])
 
         self.main_curve.canvas.draw()
         self.root.update()
 
-
-# arduino = serial.Serial('COM1', 115200, timeout=.1)
-# data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+'''
+arduino = serial.Serial('COM1', 115200, timeout=.1)
+data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+arduino.write("Hello from Python!")
+while True:
+	data = arduino.readline()
+	if data:
+		print data.rstrip('\n') #strip out the new lines for now
+'''
 c = PlotGraph()
 
 '''
